a23bf80f0e17b4f44b38614b6151a971cfbe0ee2
ezifed <joanyi9695@gmail.com>
2020-09-01 22:17:40 -0700

    Add visitor counter

--- js/index.js
+++ js/index.js
@@ -1,18 +1,31 @@
-function navmenubar(){
-  var x = document.getElementById("mymenu");
-  if(x.className === "menubar"){
-    x.className += " responsive";
-  }
-  else{
-    x.className = "menubar";
+function navmenubar() {
+  var x = document.getElementById('mymenu');
+  if (x.className === 'menubar') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'menubar';
   }
 }
 
-
-function setCursorByID(id,cursorStyle) {
+function setCursorByID(id, cursorStyle) {
   var elem;
-  if (document.getElementById &&
-     (elem=document.getElementById(id)) ) {
-   if (elem.style) elem.style.cursor=cursorStyle;
+  if (document.getElementById && (elem = document.getElementById(id))) {
+    if (elem.style) elem.style.cursor = cursorStyle;
   }
- }
+}
+
+function counter() {
+  var n = localStorage.getItem('on_load_counter');
+
+  if (n === null) {
+    n = 0;
+  }
+
+  n++;
+
+  localStorage.setItem('on_load_counter', n);
+
+  return (document.getElementById('visiter-counter').innerText =
+    'You are the ' + n + 'visitor.');
+}
+counter();
