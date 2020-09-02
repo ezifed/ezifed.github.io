c1676dec06dab04b0a010acb64032381761351df
ezifed <joanyi9695@gmail.com>
2020-09-01 20:47:20 -0700

    Add onchange feature when filing first name

--- js/contactus.js
+++ js/contactus.js
@@ -1,20 +1,24 @@
-function validateForm(){
-  var x = document.forms['myForm']["fname"].value;
-  if (x==""){
-    alert ("Please enter your first name");
+function validateForm() {
+  var x = document.forms['myForm']['fname'].value;
+  if (x == '') {
+    alert('Please enter your first name');
     return false;
   }
 
-  var y = document.forms['myForm']["lname"].value;
-  if (y==""){
-    alert ("Please enter your last name");
-    return false;
-  }
-  
-  var z = document.forms['myForm']["email"].value;
-  if (z==""){
-    alert ("Please enter your email address");
+  var y = document.forms['myForm']['lname'].value;
+  if (y == '') {
+    alert('Please enter your last name');
     return false;
   }
 
+  var z = document.forms['myForm']['email'].value;
+  if (z == '') {
+    alert('Please enter your email address');
+    return false;
+  }
+}
+
+function welcome() {
+  var x = document.getElementById('fname').value;
+  document.getElementById('welcome-name').innerText = 'Hello ' + x;
 }
